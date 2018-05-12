82a40056227bf40a3eb2862dea1f786cfc26b628
Mike Francis <mikeffrancis@gmail.com>
2018-05-12 09:32:09 +0100

    Allow AWS_URL

--- config/volumes.php
+++ config/volumes.php
@@ -3,7 +3,7 @@
 return [
     's3' => [
         'hasUrls' => true,
-        'url' => "https://${getenv('AWS_BUCKET')}.s3.amazonaws.com",
+        'url' => getenv('AWS_URL'),
         'keyId' => getenv('AWS_KEY'),
         'secret' => getenv('AWS_SECRET'),
         'bucket' => getenv('AWS_BUCKET'),
